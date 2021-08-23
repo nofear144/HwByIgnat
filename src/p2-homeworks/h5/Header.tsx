e93093e9e7ea9f0779301b6edb08efea96769b2b
nofear144 <nofear144@mail.ru>
2021-08-23 18:53:03 +0300

    Finished HW #5 by Ignat

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -4,34 +4,34 @@ import {PATH} from "./Routes";
 import s from "./pages/styles.module.css"
 
 
-
-export function Header()
-{
-    let[toogle,setToggle]=useState(false)
+export function Header() {
+    /*let[toogle,setToggle]=useState(false)
 
     const toogleMenu = ()=>{setToggle(!toogle)}
     const toogleOpen= ()=> {setToggle(true)}
-    const toogleClose = () => {setToggle(false)}
+    const toogleClose = () => {setToggle(false)}*/
 
-            return (
-                <div>
-                    <div >
-                        <button
-                            className= {s.button}
-                            onClick={toogleMenu}
-                            onMouseEnter={toogleOpen}
-                            onMouseLeave={toogleClose}
-                            >
-                        </button>
-                        {toogle &&
-                        <nav className={s.menu}>
-                            <NavLink className={s.preJun} to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
-                            <NavLink className={s.jun} to={PATH.JUNIOR}>Junior</NavLink>
-                            <NavLink className={s.junPlus} to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
-                        </nav>
-                        }
-                    </div>
-                </div>
+    return (
+        <div>
 
-            )
+            <div className={s.menu}>
+                {/*{toogle &&*/}
+
+                <NavLink className={s.preJun} to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+                <NavLink className={s.jun} to={PATH.JUNIOR}>Junior</NavLink>
+                <NavLink className={s.junPlus} to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+
+
+                {/*}*/}
+                <button
+                    className={s.button}
+                    //onClick={toogleMenu}
+                    // onMouseEnter={toogleOpen}
+                    // onMouseLeave={toogleClose}
+                >
+                </button>
+            </div>
+        </div>
+
+    )
 }
